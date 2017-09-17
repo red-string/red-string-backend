9d633007731f1bc4f89287a2ef6f613361d94c88
Ryan Kelley <rtkelley@gmail.com>
2017-09-17 12:20:17 -0400

    got regex tag array working! will add more regex filters for locations and such but it's working for now

--- RegEx/regex-practice.js
+++ RegEx/regex-practice.js
@@ -1,3 +1,4 @@
+//regular expressions for filtering out US phone numbers, web addresses and email addresses, US state abbreviations
 const phoneRegEx = /(\(?\d{3}\)?|\d{3})?( |-|\.)?(\d{3}( |-|\.)?\d{4})/gi;
 const webRegEx = /([(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256})\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
 const stateRegEx1 = /\b((A[AEKLPRSZ])|(C[AOT])|(D[EC])|(F[LM])|(G[AU])|(HI)|(I[ADLN])|(K[SY])|(L[A])|(M[ADEINOST]))[\s,.]?\b/g;
@@ -6,20 +7,23 @@ const emailRegEx = /\b[A-Z0-9._%+-]+@(?:[A-Z0-9-]+\.)+[A-Z]{2,6}\b/gi;
 // const zipRegEx = ;
 // const
 
-let regExArr = [stateRegEx1, stateRegEx2, emailRegEx, webRegEx, phoneRegEx]
+//array of regular expressions to iterate over in matching function
+const regExArr = [stateRegEx1, stateRegEx2, emailRegEx, webRegEx, phoneRegEx]
 let inputStr = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789 _+-.,!@#$%^&*();\/|<> 12345 -98.7 3.141 .6180 9,000 +42 (413) 429-6507 555.123.4567 +1-(800)-555-2468 foo@demo.net	bar.ba@test.co.uk www.demo.com john@gmail.com	http://foo.co.uk/ http://regexr.com/foo.html?q=bar https://mediatemple.net";
 let tagArr = [];
 
+//function to return array of matches which will later serve as tags
 function returnRegExTags () {
   input = inputStr;
-  console.log('input: ' + input)
   let matchArr = [];
   regExArr.forEach(function(item) {
-    matchArr = input.match(item);
-    matchArr.forEach(function(item) {
-      tagArr.push(item);
+    if (input.match(item)) {
+      matchArr = input.match(item);
+      matchArr.forEach(function(item) {
+        tagArr.push(item);
     })
     matchArr = [];
+  }
   });
   console.log(tagArr);
   return tagArr;
