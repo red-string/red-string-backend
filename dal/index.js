09ee0f87fbf7d5192af1ab5a8fb39a87b26a399d
Stephen Boynton <boyntonsc@gmail.com>
2017-10-17 13:06:50 -0400

    Package json error fix

--- dal/index.js
+++ dal/index.js
@@ -1,34 +1,35 @@
 const {
-	getMultipleFiles,
-	getFilesThatShareTag,
-	getTagById,
-	getFileById,
-	getCaseById,
-	getLastTagId,
-	getLastFileId,
-	getLastCaseId,
-	getAllTagsFromCase,
-	getAllTagsFromFile,
-	getAllFilesFromCase,
-	getAllCases
+  getMultipleFiles,
+  getFilesThatShareTag,
+  getTagById,
+  getFileById,
+  getCaseById,
+  getLastTagId,
+  getLastFileId,
+  getLastCaseId,
+  getAllTagsFromCase,
+  getAllTagsFromFile,
+  getAllFilesFromCase,
+  getAllCases
 } = require("./query");
 
 const { createTags, createFile, createCase } = require("./store");
 
 module.exports = {
-	getMultipleFiles,
-	getFilesThatShareTag,
-	getTagById,
-	getFileById,
-	getCaseById,
-	getLastTagId,
-	getLastFileId,
-	getLastCaseId,
-	getAllTagsFromCase,
-	getAllTagsFromFile,
-	getAllFilesFromCase,
-	getAllCases,
-	createTags,
-	createFile,
-	createCase
+  getMultipleFiles,
+  getFilesThatShareTag,
+  getTagById,
+  getFileById,
+  getCaseById,
+  getLastTagId,
+  getLastFileId,
+  getLastCaseId,
+  getAllTagsFromCase,
+  getAllTagsFromFile,
+  getAllFilesFromCase,
+  getAllCases,
+  createTags,
+  createFile,
+  createCase,
+  create
 };
