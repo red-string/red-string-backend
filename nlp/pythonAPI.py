0385f0b6a1f8ac890d7d2f538fa4fcc7a2b07b2c
Ryan Kelley <rtkelley@gmail.com>
2017-10-20 09:17:07 -0400

    working on pyshell

---
+++ nlp/pythonAPI.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from paralleldots import ner, taxonomy, keywords, get_api_key
+
+import sys, json, numpy as np
+
+def main():
+    thing = get_api_key()
+    print("keeeeyyyyy", thing)
+    text = sys.stdin.read()
+
+    print text
+    # text = ner(doc)
+    print("this is not where the problem is")
+    # print(text)
+    # entities = text['entities']
+    # print("entities, also not a problem", entities)
+    # tags = []
+    #
+    # for item in entities:
+    #     print("printing names = not a problem", item['name'])
+    #     tags.append(item['name'])
+    #
+    #
+    # #return the sum to the output stream
+    # print(tags)
+
+#start process
+if __name__ == '__main__':
+    main()
