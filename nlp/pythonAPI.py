d0012f5efb31fa81691a481d07caa030429a5900
Ryan Kelley <rtkelley@gmail.com>
2017-10-20 13:00:43 -0400

    updating python tag function

--- nlp/pythonAPI.py
+++ nlp/pythonAPI.py
@@ -9,19 +9,20 @@ def main():
     thing = get_api_key()
     text = sys.stdin.read()
     nerText = ner(text)
+    print nerText
     # taxText = taxonomy(text)
     # keyText = keywords(text)
-    # print(taxText)
-    # print(keyText)
-    entities = nerText['entities']
-    tags = []
-
-    for item in entities:
-        tags.append(item['name'])
-
-
-    #return the tags to the output stream
-    print(tags)
+    # # print(taxText)
+    # # print(keyText)
+    # entities = nerText['entities']
+    # tags = []
+    #
+    # for item in entities:
+    #     tags.append(item['name'])
+    #
+    #
+    # #return the tags to the output stream
+    # print(tags)
 
 #start process
 if __name__ == '__main__':
