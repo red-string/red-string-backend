005061c8386103f44cb5cc43e981abe3b44aa95f
Stephen Boynton <boyntonsc@gmail.com>
2017-11-07 12:53:22 -0500

    Order of group by

--- dal/query.js
+++ dal/query.js
@@ -135,7 +135,7 @@ function getAllTagsThatShareFile(fileId) {
     .from("Tags", "Files")
     .where("Tags.file_id", fileId)
     .join("Files", "Tags.file_id", "Files.id")
-    .groupBy("Files.id", "Tags.id")
+    .groupBy("Tags.id", "Files.id")
     .then(response => {
       console.log("Get all tags that share file", response);
       return response;
